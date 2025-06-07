bb45d7229d1d24f8271d9b4725d094b98b04a0ae
Joinsung <leeming01@naver.com>
2025-06-07 17:15:26 +0900

    🫧modify: 경로 하드코딩 제거 및 엔드포인트 상수 적용

--- src/app/shared/lib/axios.ts
+++ src/app/shared/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { useAuthStore } from '@/app/features/auth/store/auth-store'
+import { AUTH_ENDPOINT } from '@/app/features/auth/api/auth-endpoint'
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -8,7 +9,7 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const token = useAuthStore.getState().accessToken
-    const publicPaths = ['/15-2/auth/login', '/15-2/users']
+    const publicPaths = [AUTH_ENDPOINT.LOGIN, AUTH_ENDPOINT.SIGNUP]
 
     if (!publicPaths.includes(config.url || '') && token) {
       config.headers.Authorization = `Bearer ${token}`
