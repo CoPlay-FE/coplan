500b31cecc80fb559ae3d15e9a7de4fc04785b48
jyn <spdlqjdkslel1226@naver.com>
2025-06-10 03:55:31 +0900

    ✨ Feat: 버튼 추가 / Column UI

--- src/app/dashboard/[id]/Column/Column.tsx
+++ src/app/dashboard/[id]/Column/Column.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import useCards from '@/app/api/useCards'
 import type { Column } from '@/app/api/useColumns'
 
@@ -9,9 +11,27 @@ export default function Column({ column }: { column: Column }) {
   if (error) return <p>error...{error.message}</p>
 
   return (
-    <div className="BG-gray Border-column w-354 shrink-0 p-20">
-      <h2>{title}</h2>
-      <span>totalCount of cards: {data?.totalCount}</span>
+    <div className="BG-gray Border-column flex w-354 shrink-0 flex-col gap-16 p-20">
+      <div className="mb-24 flex items-center justify-between">
+        <div className="flex items-center">
+          <div className="mb-7 mr-8 size-8 rounded-25 bg-blue-500"></div>
+          <h2 className="mr-12 text-18 font-bold leading-tight">{title}</h2>
+          <span className="Text-gray rounded-4 bg-[#EEEEEE] px-6 pb-3 pt-5 text-12 font-medium leading-tight dark:bg-[#2E2E2E]">
+            {data?.totalCount}
+          </span>
+        </div>
+        <Image src={'/images/config.svg'} alt="설정" width={20} height={20} />
+      </div>
+      <button className="BG-white Border-btn rounded-6 px-146 py-9">
+        <div className="flex h-22 w-22 items-center justify-center rounded-4 bg-blue-100">
+          <Image
+            src={'/images/plus.svg'}
+            alt="플러스 아이콘"
+            width={10}
+            height={10}
+          />
+        </div>
+      </button>
       {data?.cards.map((card) => <Card key={card.id} card={card} />)}
     </div>
   )
