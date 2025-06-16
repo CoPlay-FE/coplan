e930b9564a60281b27159c6fe1593bff04425c5e
Joinsung <leeming01@naver.com>
2025-06-16 17:10:38 +0900

    🫧modify: getValuse로 변경

--- src/app/features/auth/hooks/useConfirmPasswordValidation.ts
+++ src/app/features/auth/hooks/useConfirmPasswordValidation.ts
@@ -1,14 +1,10 @@
-import { Control, useWatch } from 'react-hook-form'
-
 import { SignupFormData } from '../types/auth.type'
 
-export function useConfirmPasswordValidation(control: Control<SignupFormData>) {
-  const password = useWatch({
-    control,
-    name: 'password',
-  })
+export function useConfirmPasswordValidation(getValues: () => SignupFormData) {
   return {
-    validate: (value: string) =>
-      value === password || '비밀번호가 일치하지 않습니다',
+    validate: (value: string) => {
+      const password = getValues().password
+      return value === password || '비밀번호가 일치하지 않습니다'
+    },
   }
 }
