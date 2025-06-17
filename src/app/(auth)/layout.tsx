9cfab6bf9cc6a1032dceb8e3c540d39fcf3bdc20
Joinsung <leeming01@naver.com>
2025-06-17 19:23:54 +0900

    🐛fix: 레이아웃 반응형 수정

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="flex min-h-screen w-full items-center justify-center">
-      <div className="flex w-520 flex-col items-center justify-center gap-24 px-12 sm:px-0">
+      <div className="flex w-520 flex-col items-center justify-center gap-24 px-12 mobile:px-0">
         {children}
       </div>
     </div>
