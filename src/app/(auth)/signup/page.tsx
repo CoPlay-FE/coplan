4dcacbabe72103f2a6521421f7ad0099e573e388
Joinsung <leeming01@naver.com>
2025-06-17 18:28:23 +0900

    ✨feat: 회원가입 페이지 구현

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,10 +1,20 @@
 'use client'
 
+import Link from 'next/link'
+
+import AuthLogo from '@/app/features/auth/components/AuthLogo'
 import SignupForm from '@/app/features/auth/components/SignupForm'
 export default function Signup() {
   return (
     <>
+      <AuthLogo text="환영합니다!" />
       <SignupForm />
+      <p className="text-16 font-normal">
+        이미 회원이신가요?{' '}
+        <Link className="Text-violet underline" href="/login">
+          로그인
+        </Link>
+      </p>
     </>
   )
 }
