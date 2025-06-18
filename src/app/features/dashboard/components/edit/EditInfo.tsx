87fab0b7db521c6c6e8d136478d342e46bf3afaa
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-06-18 19:26:29 +0900

    🫧modify: 불필요한 import 제거

--- src/app/features/dashboard/components/edit/EditInfo.tsx
+++ src/app/features/dashboard/components/edit/EditInfo.tsx
@@ -1,7 +1,7 @@
 import api from '@lib/axios'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 import { DASHBOARD_COLORS } from '@/app/shared/constants/colors'
 import { CreateDashboardRequest } from '@/app/shared/types/dashboard'
