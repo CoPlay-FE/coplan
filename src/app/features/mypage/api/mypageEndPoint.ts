dd14548d5874e8f936c9c3a9acec28d8003f4032
Joinsung <leeming01@naver.com>
2025-06-19 06:14:28 +0900

    ✨feat: 비밀번호 변경 엔드 포인트 추가

--- src/app/features/mypage/api/mypageEndPoint.ts
+++ src/app/features/mypage/api/mypageEndPoint.ts
@@ -1,4 +1,5 @@
 export const MYPAGE_ENDPOINT = {
   USER: `/${process.env.NEXT_PUBLIC_TEAM_ID}/users/me`,
   IMAGE: `/${process.env.NEXT_PUBLIC_TEAM_ID}/users/me/image`,
+  CHANGE_PASSWORD: `/${process.env.NEXT_PUBLIC_TEAM_ID}/auth/password`,
 }
