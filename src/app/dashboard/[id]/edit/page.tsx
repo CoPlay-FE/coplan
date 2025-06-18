785028e105c04224b3ef048b08e45b7e4cc28073
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-06-18 15:27:48 +0900

    ✨feat: 대시보드 수정 페이지 작성

--- src/app/dashboard/[id]/edit/page.tsx
+++ src/app/dashboard/[id]/edit/page.tsx
@@ -1,24 +1,37 @@
 'use client'
 
+import EditInfo from '@dashboard/components/edit/EditInfo'
+import EditInvitation from '@dashboard/components/edit/EditInvitation'
+import EditMember from '@dashboard/components/edit/EditMember'
 import { showError, showSuccess } from '@lib/toast'
+import Image from 'next/image'
 
 export default function DashBoardEditPage() {
   const handleSuccess = () => {
-    showSuccess('대시보드가 성공적으로 저장되었습니다.')
+    showSuccess('대시보드가 성공적으로 수정되었습니다.')
   }
 
   const handleError = () => {
-    showError('저장 중 오류가 발생했습니다.')
+    showError('수정 중 오류가 발생했습니다.')
   }
 
   return (
-    <div className="space-y-4 p-6">
-      <p className="text-xl font-semibold">대시보드 수정 페이지</p>
-
-      <div className="flex gap-4">
-        <button onClick={handleSuccess}>성공 토스트</button>
-        <button onClick={handleError}>에러 토스트</button>
+    <div className="BG-gray">
+      <div
+        className="flex cursor-pointer items-center gap-12 p-16"
+        onClick={() => window.history.back()}
+      >
+        <Image src="/images/back.png" alt="돌아가기" width={8} height={4} />
+        <p>돌아가기</p>
       </div>
+      <div className="flex w-500 flex-col gap-16 p-16">
+        <EditInfo />
+        <EditMember />
+        <EditInvitation />
+      </div>
+      <button className="Text-btn Border-btn ml-16 rounded-md px-64 py-12">
+        대시보드 삭제하기
+      </button>
     </div>
   )
 }
