dab06eebbb62d953a65d18305919cdfa24f28385
Joinsung <leeming01@naver.com>
2025-06-19 01:27:20 +0900

    ✨feat: 마이페이지 이미지 업로드 API 함수 추가

--- src/app/features/mypage/api/mypageApi.ts
+++ src/app/features/mypage/api/mypageApi.ts
@@ -2,7 +2,10 @@ import api from '@lib/axios'
 
 import { User as UserDataResponse } from '@/app/shared/types/user.type'
 
-import { UpdateProfileRequest } from '../types/mypage.type'
+import {
+  UpdateProfileRequest,
+  UploadProfileImageResponse,
+} from '../types/mypage.type'
 import { MYPAGE_ENDPOINT } from './mypageEndPoint'
 
 export async function loadUser(): Promise<UserDataResponse> {
@@ -13,6 +16,18 @@ export async function loadUser(): Promise<UserDataResponse> {
 export async function updateMyProfile(
   data: UpdateProfileRequest,
 ): Promise<UserDataResponse> {
-  const response = await api.post<UserDataResponse>(MYPAGE_ENDPOINT.USER, data)
+  const response = await api.put<UserDataResponse>(MYPAGE_ENDPOINT.USER, data)
+  return response.data
+}
+
+export async function uploadProfileImage(
+  image: File,
+): Promise<UploadProfileImageResponse> {
+  const formData = new FormData()
+  formData.append('image', image)
+  const response = await api.post<UploadProfileImageResponse>(
+    MYPAGE_ENDPOINT.IMAGE,
+    formData,
+  )
   return response.data
 }
