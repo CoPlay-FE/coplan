60f5c41ab2ef3ec451a4fadfc37dbf6248f2e55f
Joinsung <leeming01@naver.com>
2025-06-19 06:17:30 +0900

    ✨feat: 새 비밀번호 중복 검증 훅 구현

---
+++ src/app/features/mypage/hook/useNewPasswordValidation.ts
@@ -0,0 +1,13 @@
+import { mypageValidation } from '../schemas/mypageValidation'
+
+export function useNewPasswordValidation(getPasswordValue: () => string) {
+  return {
+    ...mypageValidation.password,
+    validate: (value: string) => {
+      if (value === getPasswordValue()) {
+        return '현재 비밀번호와 동일한 비밀번호입니다.'
+      }
+      return true
+    },
+  }
+}
