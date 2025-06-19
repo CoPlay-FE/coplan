2081282ab5bcdf4cebcf822a812e0453b400eac6
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-06-19 21:57:14 +0900

    ✨feat: link와 button 컴포넌트의  재사용성과 가독성 향상을 위한 컴포넌트 작성

--- src/app/shared/components/common/header/RightHeaderNav.tsx
+++ src/app/shared/components/common/header/RightHeaderNav.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import { cn } from '@lib/cn'
 import { useModalStore } from '@store/useModalStore'
 import { useSelectedDashboardStore } from '@store/useSelectedDashboardStore'
-import Image from 'next/image'
-import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+
+import NavItem from './NavItem'
 
 export default function RightHeaderNav() {
   const pathname = usePathname()
@@ -14,30 +13,20 @@ export default function RightHeaderNav() {
 
   return (
     <nav className="Text-gray hidden gap-8 text-sm md:flex">
-      <Link
+      <NavItem
+        as="link"
         href={`/dashboard/${selectedDashboard?.id}/edit`}
-        className={cn(
-          'Border-btn flex items-center gap-6 rounded-md border px-12 py-6',
-          pathname === '/dashboard' && 'font-semibold',
-        )}
-      >
-        <div className="relative flex size-12">
-          <Image src="/images/management.png" fill alt="관리 버튼" />
-        </div>
-        관리
-      </Link>
-      <button
+        iconSrc="/images/management.png"
+        label="관리"
+        active={pathname.includes('/edit')}
+      />
+      <NavItem
+        as="button"
         onClick={() => openModal('invite')}
-        className={cn(
-          'Border-btn mr-16 flex items-center gap-6 rounded-md px-12 py-6',
-          modalType === 'invite' && 'font-semibold',
-        )}
-      >
-        <div className="relative flex size-12">
-          <Image src="/images/invitation.png" fill alt="초대 버튼" />
-        </div>
-        초대하기
-      </button>
+        iconSrc="/images/invitation.png"
+        label="초대하기"
+        active={modalType === 'invite'}
+      />
     </nav>
   )
 }
