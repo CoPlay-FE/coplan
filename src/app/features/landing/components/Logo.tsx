4a590844800f9852cf483fc0f375fa2683f89097
Joinsung <leeming01@naver.com>
2025-06-21 03:09:23 +0900

    ✨feat: Logo 컴포넌트 구현

---
+++ src/app/features/landing/components/Logo.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { useMounted } from '@hooks/useMounted'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useTheme } from 'next-themes'
+
+export default function Logo() {
+  const { resolvedTheme } = useTheme()
+  const mounted = useMounted()
+
+  const isDark = resolvedTheme === 'dark'
+
+  const mobileLogoSrc = mounted
+    ? isDark
+      ? '/images/logo-icon-dark.svg'
+      : '/images/logo-icon-light.svg'
+    : '/images/logo-icon-light.svg'
+
+  const desktopLogoSrc = mounted
+    ? isDark
+      ? '/images/logo-dark2.svg'
+      : '/images/logo-light2.svg'
+    : '/images/logo-light2.svg'
+
+  return (
+    <Link href="/" className="mobileS:w-30 mobileS:h-30 relative h-30 w-120">
+      {/* 모바일용 로고 */}
+      <Image
+        suppressHydrationWarning
+        className="mobileS:block hidden object-contain"
+        src={mobileLogoSrc}
+        fill
+        alt="모바일 로고"
+      />
+
+      {/* 데스크탑/태블릿용 로고 */}
+      <Image
+        suppressHydrationWarning
+        className="mobileS:hidden block object-contain"
+        src={desktopLogoSrc}
+        fill
+        alt="일반 로고"
+      />
+    </Link>
+  )
+}
