6d26ed5c6d52bd6c15efc390419e779dc518cfc0
Joinsung <leeming01@naver.com>
2025-06-21 03:35:49 +0900

    🎨style: 일부 누락 스타일 수정

--- src/app/features/landing/components/Footer.tsx
+++ src/app/features/landing/components/Footer.tsx
@@ -2,7 +2,7 @@ import GithubIcon from './GithubIcon'
 
 export default function Footer() {
   return (
-    <footer className="tablet-wide:h-100 tablet-wide:flex-row tablet-wide:justify-between tablet-wide:px-140 flex h-236 flex-col items-center justify-center gap-32">
+    <footer className="tablet-wide:h-100 tablet-wide:flex-row mobile-wide:flex-col tablet-wide:justify-between tablet-wide:px-40 flex h-236 items-center justify-between gap-32 px-140">
       <p className="font-bold">@Coplan - 2025</p>
       <GithubIcon />
     </footer>
--- src/app/features/landing/components/Main.tsx
+++ src/app/features/landing/components/Main.tsx
@@ -4,7 +4,7 @@ import PointSection from './PointSection'
 
 export default function Main() {
   return (
-    <main className="mobileS:mb-120 mobileS:mt-42 mb-160 mt-94">
+    <main className="mobile-wide:mb-120 mobile-wide:mt-42 mb-160 mt-94">
       <HeroSection />
       <div className="flex flex-col items-center justify-center">
         <PointSection />
