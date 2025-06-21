ade72c4784bae598b59ffd52f67093694c7792ac
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-06-22 08:03:13 +0900

    🎨style: 마이페이지인 경우 계정관리가 보이도록 수정

--- src/app/shared/components/common/header/LeftHeaderContent.tsx
+++ src/app/shared/components/common/header/LeftHeaderContent.tsx
@@ -1,19 +1,24 @@
 'use client'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 
 import { useSelectedDashboardStore } from '@/app/shared/store/useSelectedDashboardStore'
 
 export default function LeftHeaderContent() {
   const { selectedDashboard } = useSelectedDashboardStore()
+  const pathname = usePathname()
 
   if (!selectedDashboard) return null
 
   return (
     <div className="flex shrink-0 items-center gap-8 pr-16">
       <div className="whitespace-nowrap font-bold">
-        {selectedDashboard.title || '내 대시보드'}
+        {pathname === '/mypage'
+          ? '계정관리'
+          : selectedDashboard.title || '내 대시보드'}
       </div>
-      {selectedDashboard.createdByMe && (
+
+      {selectedDashboard.createdByMe && pathname !== '/mypage' && (
         <div className="relative h-12 w-14 overflow-hidden">
           <Image
             src="/images/crown.png"
