48224dd82dce227cbbcc8e9b6fa2fed661913d02
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-06-23 08:50:05 +0900

    ♻️refactor: 제목과 페이지네이션 컴포넌트 분리 및 반응형 적용

--- src/app/features/dashboard/components/edit/PaginationHeader.tsx
+++ src/app/features/dashboard/components/edit/PaginationHeader.tsx
@@ -6,7 +6,6 @@ import React from 'react'
 type PaginationHeaderProps = {
   currentPage: number
   totalPages: number
-  title: string
   onPrev: () => void
   onNext: () => void
   children?: React.ReactNode
@@ -15,44 +14,45 @@ type PaginationHeaderProps = {
 export function PaginationHeader({
   currentPage,
   totalPages,
-  title,
   onPrev,
   onNext,
 }: PaginationHeaderProps) {
   return (
-    <div className="mb-20 flex max-w-500 items-center justify-between whitespace-nowrap">
-      <h2 className="Text-black text-18 font-bold">{title}</h2>
+    <div className="flex max-w-500 items-center justify-center">
+      <p className="Text-gray mx-16 text-12 sm:mx-32 mobile-sm:text-10">
+        {totalPages} 페이지 중 {currentPage}
+      </p>
 
-      <div className="flex shrink-0 items-center">
-        <p className="Text-gray mx-16 shrink-0 text-12 sm:mx-32">
-          {totalPages} 페이지 중 {currentPage}
-        </p>
-
-        <button onClick={onPrev} disabled={currentPage === 1}>
-          <Image
-            src={
-              currentPage === 1
-                ? '/images/prev-disabled.png'
-                : '/images/prev.png'
-            }
-            alt="이전"
-            width={36}
-            height={36}
-          />
-        </button>
-        <button onClick={onNext} disabled={currentPage === totalPages}>
-          <Image
-            src={
-              currentPage === totalPages
-                ? '/images/next-disabled.png'
-                : '/images/next.png'
-            }
-            alt="다음"
-            width={36}
-            height={36}
-          />
-        </button>
-      </div>
+      <button
+        onClick={onPrev}
+        disabled={currentPage === 1}
+        className="relative mobile-sm:size-30"
+      >
+        <Image
+          src={
+            currentPage === 1 ? '/images/prev-disabled.png' : '/images/prev.png'
+          }
+          alt="이전"
+          width={36}
+          height={36}
+        />
+      </button>
+      <button
+        onClick={onNext}
+        disabled={currentPage === totalPages}
+        className="relative mobile-sm:size-30"
+      >
+        <Image
+          src={
+            currentPage === totalPages
+              ? '/images/next-disabled.png'
+              : '/images/next.png'
+          }
+          alt="다음"
+          width={36}
+          height={36}
+        />
+      </button>
     </div>
   )
 }
