bf9135eef8fd17987c7dd8442a271e51d3dcbc81
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-06-23 03:10:00 +0900

    🐛fix: 모바일에서 관리 버튼이 보이지 않는 버그 수정

--- src/app/shared/components/common/header/Header.tsx
+++ src/app/shared/components/common/header/Header.tsx
@@ -9,21 +9,21 @@ import UserDropdown from './UserDropdown'
 
 export default function Header() {
   return (
-    <header className="BG-white Border-bottom Text-black w-full overflow-x-auto border-b px-48 py-10">
-      <div className="flex w-full items-center justify-between pr-16">
+    <header className="BG-white Border-bottom Text-black w-full overflow-x-auto border-b px-24 py-6 sm:px-48 sm:py-10">
+      <div className="flex min-w-[max-content] items-center justify-between pr-8 sm:pr-16">
         {/* 좌측 대시보드명 */}
         <div className="hidden lg:block">
           <LeftHeaderContent />
         </div>
 
         {/* 우측 사용자 정보/다크모드 */}
-        <div className="flex gap-16 whitespace-nowrap">
+        <div className="flex gap-8 whitespace-nowrap sm:gap-16">
           <RightHeaderNav />
           <CreateInvitationModal />
           {/* 협업자 목록 */}
           <CollaboratorList />
 
-          <div className="flex items-center gap-32 border-l pl-16">
+          <div className="flex items-center gap-16 border-l pl-8 sm:gap-32 sm:pl-16">
             {/* 사용자 정보 드롭다운 */}
             <UserDropdown />
             {/* 다크모드 토글 */}
